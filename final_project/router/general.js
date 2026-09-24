e077b797e8d353c40e8d946d536e8d9f26cd6036
Raphael Lopes <ph.campanario07@gmail.com>
2026-09-24 11:01:19 -0400

    task 3: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { author } = req.params;
+  const data = [];
+  const bookArray = Object.entries(books);
+  for (const [_, book] of bookArray) {
+    if (book.author.toLocaleLowerCase() === author.toLocaleLowerCase()) {
+        data.push(book);
+    }
+  }
+  
+  return res.status(200).send(JSON.stringify(data, null, 2));
 });
 
 // Get all books based on title
