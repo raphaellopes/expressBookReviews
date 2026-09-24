1fc84649afa6bcf1eb42aa96eee9b4a396a33244
Raphael Lopes <ph.campanario07@gmail.com>
2026-09-24 10:42:36 -0400

    task 2: getting book from isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).send(`No book found for ISBN ${isbn}`);
+  }
+
+  return res.status(200).send(JSON.stringify(book, null, 2));
  });
   
 // Get book details based on author
