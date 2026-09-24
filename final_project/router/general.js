4eb2d78af3a52a52d566ae53c754bf1a39bbd76d
Raphael Lopes <ph.campanario07@gmail.com>
2026-09-24 12:05:37 -0400

    task 5: getting reviews by book isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -54,8 +54,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).send(`No book found with isbn ${isbn}`);
+  }
+
+  const { reviews = {} } = book;
+  return res.status(200).send(JSON.stringify(reviews, null, 2));
 });
 
 module.exports.general = public_users;
